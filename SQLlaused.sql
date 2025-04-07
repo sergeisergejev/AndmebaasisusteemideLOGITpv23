6b17a602c692088acbca7987565b3ac580c71ac0
sergeisergejev <sergaysergaev@gmail.com>
2025-04-07 11:39:20 +0300

    SQL lausede uuendamine

--- SQLlaused.sql
+++ SQLlaused.sql
@@ -6,6 +6,7 @@
 --1. Windows Auth - localdb admini õigused
 --2. SQL Server Auth - kontrollida varem tehtud kasutajad
 
+
 CREATE DATABASE sergejevLOGITpv23;
 USE sergejevLOGITpv23;
 
@@ -22,3 +23,21 @@ opilaskodu bit
 SELECT * FROM inimene;
 --tabeli kustutamine
 DROP table inimene;
+
+--andmete lisamine
+--DDL - data definition language
+--DML - data manipulation language
+
+INSERT INTO inimene
+(nimi, sunniaeg, telefon, pikkus, opilaskodu)
+values
+('Peeter Uus', '2021-12-30', '9564735', 85.5, 0),
+('Mari Speek', '1980-10-20', '778795', 174.5, 0),
+('Tonu Talve', '2075-05-10', '656295', 180.5, 0),
+('Peeter Oja', '2005-11-17', '2865995', 182.5, 1)
+;
+
+select * from inimene;
+
+delete from inimene
+where inimeneID=3;
