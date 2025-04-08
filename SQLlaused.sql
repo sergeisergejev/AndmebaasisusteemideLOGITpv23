1a7a88c08f29c2711ad8663ff353716f5fe0f7e8
sergeisergejev <sergaysergaev@gmail.com>
2025-04-08 13:17:50 +0300

    tabelitega diagrammi loomine

--- SQLlaused.sql
+++ SQLlaused.sql
@@ -40,4 +40,77 @@ values
 select * from inimene;
 
 delete from inimene
-where inimeneID=3;
+where inimeneID=7;
+
+--tabel elukoht
+create table elukoht(
+elukohtID int PRIMARY KEY identity(1,1),
+elukoht varchar(50) unique,
+maakond varchar(50)
+);
+
+select * from elukoht;
+--andmete lisamine tabeli elukoht
+insert into elukoht(elukoht, maakond)
+values ('Tallinn', 'Harjumaa'),
+('Keila', 'Harjumaa'),
+('Pärnu', 'Pärnumaa')
+;
+--tabeli muutmine uue veergu lisamine
+alter table inimene add elukohtID int;
+
+select * from inimene;
+select * from elukoht;
+
+--foreign key lisamine
+alter table inimene add constraint fk_elukoht
+foreign key (elukohtID)
+references elukoht(elukohtID);
+
+INSERT INTO inimene
+(nimi, sunniaeg, telefon, pikkus, opilaskodu, elukohtID)
+values
+('Peeter Sir', '2021-12-30', '2564735', 82.5, 0, 2);
+
+select * from inimene join elukoht
+ON inimene.elukohtID=elukoht.elukohtID;
+
+select i.nimi, i.sunniaeg, e.elukoht, e.maakond 
+from inimene i join elukoht e
+ON i.elukohtID=e.elukohtID;
+
+
+CREATE TABLE autod(
+autodID int Primary Key identity(1,1),
+autoNR char(12) unique,
+mudell char(12),
+mark char(12),
+v_aasta int
+);
+
+select * from inimene;
+select * from autod;
+
+insert into autod
+(autoNR, mudell, mark, v_aasta)
+values
+('EST111', 'Audi', 'Q7', '2016'),
+('TSE131', 'Audi', 'A8', '2007');
+
+alter table inimene add autodID int;
+
+alter table inimene add constraint fk_autod
+foreign key (autodID)
+references autod(autodID);
+
+INSERT INTO inimene
+(nimi, sunniaeg, telefon, pikkus, opilaskodu, elukohtID, autodID)
+values
+('Peeter Surr22', '2002-12-30', '5835735', 192, 0, 2, 1);
+
+select * from inimene join elukoht
+ON inimene.elukohtID=elukoht.elukohtID;
+
+select i.nimi, i.sunniaeg, a.autoNR, a.mudell, a.mark, a.v_aasta
+from inimene i join autod a
+ON i.elukohtID=a.autodID;
